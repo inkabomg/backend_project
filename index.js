037cc8e106bf7bb69abf4d7834c7bb146a85d434
Inka Lagerbom <inka.lagerbom@tuni.fi>
2021-12-03 10:36:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 var cors = require("cors");
 app.use(cors());
 const port = process.env.PORT || 8080;
 
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
 const db = [{ name: "tiina" }, { name: "jack" }];
+
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM locations", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 app.get("/names", (req, res) => {
   res.send(db);
